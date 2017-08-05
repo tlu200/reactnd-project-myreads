4954c853f2cd0e2174d4d2b4d4f3cc0f56bbe441
tlu200 <tlu200@users.noreply.github.com>
2017-08-04 22:35:30 -0700

    Use react-router-dom  to control page content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
@@ -6,13 +7,6 @@ import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   };
 
@@ -25,13 +19,16 @@ class BooksApp extends React.Component {
   render() {
     const { books } = this.state;
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks/>
-        ) : (
-          <ListBooks books={books}/>
-        )}
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Route exact path="/" render={() => (
+            <ListBooks books={books}/>
+          )}/>
+          <Route path="/search" render={() => (
+            <SearchBooks/>
+          )}/>
+        </div>
+      </BrowserRouter>
     )
   }
 }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import Bookshelf from './Bookshelf';
 
@@ -39,7 +40,7 @@ class ListBooks extends  Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => {}}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
     )
